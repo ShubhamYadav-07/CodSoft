bcad45a3efd85c1b5cdf1bc778ed041e6a7d5785
Shubham <yadav07072005@gmail.com>
2024-10-30 09:47:53 +0530

    git reset

--- currency_converter.java
+++ currency_converter.java
@@ -8,32 +8,32 @@ public class currency_converter {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Welcome to Currency Converter!");
-        System.out.println("1. USD to Rupee");
-        System.out.println("2. Rupee to USD");
-        System.out.println("3. Euro to Rupee");
-        System.out.println("4. Rupee to Euro");
+        System.out.println("1. USD to Rupees");
+        System.out.println("2. Rupees to USD");
+        System.out.println("3. Euro to Rupees");
+        System.out.println("4. Rupees to Euro");
         System.out.print("Enter your choice (1 or 2 or 3 or 4): ");
         int choice = sc.nextInt();
 
         if (choice == 1) {
             System.out.print("Enter the amount in USD: ");
             double USD = sc.nextDouble();
-            double Rupee = usdToRupee(USD);
-            System.out.println("Converted amount in Rupee: " + Rupee);
+            double Rupees = usdToRupees(USD);
+            System.out.println("Converted amount in Rupee: " + Rupees);
         } else if (choice == 2) {
-            System.out.print("Enter the amount in Rupee: ");
-            double Rupee = sc.nextDouble();
-            double USD = RupeeToUsd(Rupee);
+            System.out.print("Enter the amount in Rupees: ");
+            double Rupees = sc.nextDouble();
+            double USD = RupeesToUsd(Rupees);
             System.out.println("Converted amount in USD: " + USD);
         } else if (choice == 3) {
             System.out.print("Enter the amount in EURO: ");
             double EURO = sc.nextDouble();
-            double Rupee = euroToRupee(EURO);
-            System.out.println("Converted amount in USD: " + Rupee);
+            double Rupees = euroToRupees(EURO);
+            System.out.println("Converted amount in USD: " + Rupees);
         } else if (choice == 4) {
-            System.out.print("Enter the amount in Rupee: ");
-            double Rupee = sc.nextDouble();
-            double EURO = RupeeToEuro(Rupee);
+            System.out.print("Enter the amount in Rupees: ");
+            double Rupees = sc.nextDouble();
+            double EURO = RupeesToEuro(Rupees);
             System.out.println("Converted amount in USD: " + EURO);
         }
 
@@ -44,20 +44,20 @@ public class currency_converter {
         sc.close();
     }
 
-    public static double usdToRupee(double USD) {
+    public static double usdToRupees(double USD) {
         return USD * 83.47; // Date: 01/05/24
     }
 
-    public static double RupeeToUsd(double Rupee) {
-        return Rupee * 0.0119804; // Date 01/05/2024
+    public static double RupeesToUsd(double Rupees) {
+        return Rupees * 0.0119804; // Date 01/05/2024
     }
 
-    public static double euroToRupee(double EURO) {
+    public static double euroToRupees(double EURO) {
         return EURO * 89.10; // Date: 01/05/24
     }
 
-    public static double RupeeToEuro(double Rupee) {
-        return Rupee * 0.0113; // Date: 01/05/24
+    public static double RupeesToEuro(double Rupees) {
+        return Rupees * 0.0113; // Date: 01/05/24
     }
 
 }
